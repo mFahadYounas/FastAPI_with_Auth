4072611172506d6fc5ccfe27a7904b85c050d33a
mFahadYounas <fahadyounas97@gmail.com>
2025-10-27 14:19:58 +0500

    Added function to verify token

--- app/security.py
+++ app/security.py
@@ -1,6 +1,7 @@
 from datetime import timedelta, datetime, timezone
-from jose import jwt
+from jose import jwt, JWTError
 from os import environ
+from fastapi import HTTPException
 
 ACCESS_TOKEN_EXPIRES_MINS = 15
 ALGORITHM = "HS256"
@@ -15,7 +16,17 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     to_encode.update({"exp": expiry})
 
     if not jwt_secret_key:
-        raise ValueError("Could not encode JWT: JWT Secret Key not available")
+        raise ValueError("Could not encode JWT: JWT Secret Key not available!")
 
     encoded_jwt = jwt.encode(to_encode, jwt_secret_key, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_token(token: str) -> dict:
+    if not jwt_secret_key:
+        raise ValueError("Could not decode JWT: JWT Secret Key not available!")
+    try:
+        payload = jwt.decode(token, jwt_secret_key, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
