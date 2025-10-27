b41ec6f68d3a1016d4265a73aa6d31213b4350c6
mFahadYounas <fahadyounas97@gmail.com>
2025-10-27 15:19:19 +0500

    Added login route

---
+++ app/api/v1/login.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter
+from app.schemas.login import LoginRequest, LoginResponse
+from app.ResponseBuilder import ResponseBuilder
+from app.security import create_access_token
+from app.schemas.api_response import APIResponse
+
+login_router = APIRouter()
+
+fake_users = {
+    "alice": {"username": "alice", "password": "secret"},
+    "bob": {"username": "bob", "password": "secret"},
+}
+
+
+@login_router.post("/login", response_model=APIResponse[LoginResponse])
+def login(request: LoginRequest):
+    user = fake_users.get(request.username)
+    if not user or user["password"] != request.password:
+        ResponseBuilder[LoginResponse].error(
+            status=401, error_msg="Invalid username or password!"
+        )
+
+    access_token = create_access_token(data={"sub": request.username})
+    response = LoginResponse(access_token=access_token, token_type="bearer")
+
+    return ResponseBuilder[LoginResponse].success(data=response)
